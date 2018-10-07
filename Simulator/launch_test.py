0bc2e1ddc8924e8246084e9e90ffcc61af1c8a57
Nicola Dainese <nicola.dainese96@gmail.com>
2018-10-07 18:15:16 +0200

    Added full code explanation to the temporary user interface of the simulator

--- Simulator/launch_test.py
+++ Simulator/launch_test.py
@@ -1,51 +1,62 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Aug 21 09:10:18 2018
-Sort of user interface used to execute all kinds of simulation of cvm.
+Sort of user interface used to execute all kinds of simulation of CVM. 
+Directly executable from Python console.
 @author: nicola.dainese96@gmail.com
 """
-from NODF_swap import NODF_swap
+from NODF_swap import NODF_swap #this code is full of trubles - not used in the thesis' version
 import one_simul_launcher as launch
-from main import tau_conv
+from main import tau_conv #this part can be improved - not all main.py file is required
 
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 #This directory at the moment has to be written by the user. 
 work_dir = 'C:/Users/Utente/Anaconda3/Cooperazione'
-#Actually more than a working directory it's the folder in which all the data from
-#a simulation are printed.
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
-N = 100
-S1 = 10
-S2 = 10
-C = 0.35
-step = 100
-P = 1 #uno su C
-rip2 = 200
-eps2 = 0.1
-dir_name = work_dir+'/NODF/'+repr(C)+'-'+repr(step)+'-'+repr(P)+'-'+repr(rip2)
+#Actually more than a working directory it's the folder in which all the data 
+#from a simulation are printed.
+N = 100 #number of individuals of each VM
+S1 = 10 #number of species of the first group - usually I set S = sqrt(N)
+S2 = 10  #number of species of the first group
+rip = 2 #number of realization for each value of the parameters - 100 suggested for statistic significance
+eps = 0.1 #epsilon - intensity of mutualism
+p = 0.35 #probability of an element matrix Mij to be 1 - on average is equal to the connectance C
+epsilons = [0.0, 0.033, 0.066, 0.1, 0.2, 0.35, 0.5, 0.65, 0.8, 1.0] #set of values for epsilon
+ps = [0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55] #set of values for p (~C)
+t = tau_conv(N) #check if it's measured in steps or generations
+#t = 150001 (time can be specified also manually)
+#Note that the result form tau_conv(N) can vary for the very same N 
+#(because basically it depends on absorption time of a single Voter Model 
+#without speciation and cooperation)
+#so this could cause some troubles in comparing two results of simulations with 
+#same input parameters but different t
 #
-rip = 2
-eps = 0.1
-p = 0.35
-epsilons = [0.0, 0.033, 0.066, 0.1, 0.2, 0.35, 0.5, 0.65, 0.8, 1.0]
-ps = [0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55]
+#parameters for NODF_launcher
+step = 100 #variables inherited from an older version - don't change it
+P = 1 #variables inherited from an older version - don't change it
+rip2 = 200 #number of realization for each value of the parameters
+C = 0.35 #connectance
+eps2 = 0.1 #epsilon - intensity of mutualism 
+#
+#at this dir_name could be added the time of the simulation, resulting from tau_conv call
+dir_name = work_dir+'/NODF/'+repr(C)+'-'+repr(step)+'-'+repr(P)+'-'+repr(rip2)
 
-#t = tau_conv(N)
-#t = 150001
-t = 7500
-commands = [0]
+commands = [0] #select [0,1,2,3,4] to execute all statements
 for x in commands:
     if x == 0:
-        
-        
+        #vary the connectance with fixed epsilon
+        #the last entry (0 or 1) specifies which kind of synthetic nested to use
+        #I suggest the 1 option because it's more balanced
         launch.p_launcher(N, rip, eps, ps, t, work_dir, 0)
+        #this option feature should be improved in a more clear way
     elif x == 1:
-        
-       
+        #vary epsilon (mutualism intensity) with fixed connectance
         launch.e_launcher(N, rip, epsilons, p, t, work_dir)
     elif x == 2:
         print('Eseguo NODF_launcher.')
-        
-        #l'ultima variabile è 'option', che per 0 fa nested2 (maggiore NODF iniziale), altrimenti nested e basta
+        #l'ultima variabile è 'option', che per 0 fa nested2 (maggiore NODF iniziale), 
+        #altrimenti nested e basta
         launch.NODF_launcher(N, S1, S2, C, eps2, step, P, rip2, t, work_dir, 0)
         #launch.NODF_launcher(N, S1, S2, C, eps2, step, P, rip2, t, work_dir, 1)
         #eps2 = 0.5
@@ -54,18 +65,23 @@ for x in commands:
         #launch.NODF_launcher(N, S1, S2, C, eps2, step, P, rip2, t, work_dir)
     elif x == 3:
         print('Eseguo precision_simul.')
-        
+        #this parameters should be local variables
+        #so they don't interfere with the statements above
         C = 0.35
         eps = 0.1
         rip = 50
+        #run for comparing (for fixed espilon and C) random and balanced nested cases 
+        #it could be extended to asymmetric nested case too
         launch.precision_simul(N, S1, S2, C, eps, rip, t, work_dir)
     elif x == 4:
+        #to be revised
         print('Eseguo NODF_swap.')
         NODF_swap(S1, S2, 0.35, step, 2, dir_name)
     else:
         continue
     
-import winsound
+import winsound #hope this library works for all operating systems and not just for windows
+#anyway it's just an alert
 duration = 500  # millisecond
 freq = 600  # Hz
 winsound.Beep(freq, duration)
